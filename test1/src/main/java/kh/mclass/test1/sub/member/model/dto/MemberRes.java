ba9b6a973758367d4fa803faeef478e18d401b34
singas0ng <juliachaewon@naver.com>
2024-05-16 08:05:41 +0900

    @Data 사용

--- test1/src/main/java/kh/mclass/test1/sub/member/model/dto/MemberRes.java
+++ test1/src/main/java/kh/mclass/test1/sub/member/model/dto/MemberRes.java
@@ -1,43 +1,21 @@
 package kh.mclass.test1.sub.member.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@ToString //generate toString 해줌
+//@AllArgsConstructor //argument 가지고 있는 생성자도 만들어줌(생성자 안에 매개인자 있는거)
+//@NoArgsConstructor //매개인자 없는 생산자
+//@Getter
+//@Setter
+@Data //위에꺼 한꺼번에 걸어줌
 public class MemberRes {
 	private String memId;
 	private String memPwd;
 	private String memEmail;
-	
-	
-	@Override
-	public String toString() {
-		return "MemberRes [memId=" + memId + ", memPwd=" + memPwd + ", memEmail=" + memEmail + "]";
-	}
-
-	public MemberRes() {
-		super();
-	}
-
-	public String getMemId() {
-		return memId;
-	}
-
-	public void setMemId(String memId) {
-		this.memId = memId;
-	}
-
-	public String getMemPwd() {
-		return memPwd;
-	}
-
-	public void setMemPwd(String memPwd) {
-		this.memPwd = memPwd;
-	}
-
-	public String getMemEmail() {
-		return memEmail;
-	}
-
-	public void setMemEmail(String memEmail) {
-		this.memEmail = memEmail;
-	}
-	
 	
 }
